32db6ef85ed52cc960008f32c52a3a4dc8e21283
xuesongde <185217985@qq.com>
2019-12-16 13:02:54 +0800

    add proxy

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 webpack4 package multiple pages
 
-https://github.com/haotian-wang/google-access-helper  install chrome extension without vpn
+https://github.com/haotian-wang/google-access-helper install chrome extension without vpn
+
+first: npm install
+
+second: npm run start
+
+then you can develop you own application
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,26 +1,30 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-const fs = require('fs')
-const entries = (pagesPath = './src/pages') => fs.readdirSync(path.resolve(pagesPath)).reduce((p, name) => {
-  if (fs.statSync(path.resolve(pagesPath, name)).isDirectory()) p[name] = `${pagesPath}/${name}/index.js`
-  return p
-}, {})
-const htmls = (pagesPath = './src/pages') => fs.readdirSync(path.resolve(pagesPath)).reduce((p, name) => {
-  console.log(name)
-  p.push(new HtmlWebpackPlugin({
-    filename: name,
-    chunks: ['venders', name.replace('.html', '')],
-    template: `${pagesPath}/${name}/index.html`,
-    favicon: './src/assets/favicon.ico'
-  }))
-  return p
-}, [])
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const fs = require("fs");
+const entries = (pagesPath = "./src/pages") =>
+  fs.readdirSync(path.resolve(pagesPath)).reduce((p, name) => {
+    if (fs.statSync(path.resolve(pagesPath, name)).isDirectory())
+      p[name] = `${pagesPath}/${name}/index.js`;
+    return p;
+  }, {});
+const htmls = (pagesPath = "./src/pages") =>
+  fs.readdirSync(path.resolve(pagesPath)).reduce((p, name) => {
+    console.log(name);
+    p.push(
+      new HtmlWebpackPlugin({
+        filename: name,
+        chunks: ["venders", name.replace(".html", "")],
+        template: `${pagesPath}/${name}/index.html`,
+        favicon: "./src/assets/favicon.ico"
+      })
+    );
+    return p;
+  }, []);
 
 const config = {
-
   // This option controls if and how source maps are generated.
   // https://webpack.js.org/configuration/devtool/
-  devtool: 'eval-cheap-module-source-map',
+  devtool: "eval-cheap-module-source-map",
 
   // https://webpack.js.org/concepts/entry-points/#multi-page-application
   entry: entries(),
@@ -28,8 +32,16 @@ const config = {
   // https://webpack.js.org/configuration/dev-server/
   devServer: {
     port: 8080,
-    contentBase: path.join(__dirname, 'dist'),
-    writeToDisk: false // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+    contentBase: path.join(__dirname, "dist"),
+    writeToDisk: false, // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+    proxy: {
+      "/api": {
+        // 转发规则
+        target: "http://localhost:3000", // 后端接口地址
+        pathRewrite: { "^/api": "" }, // 路径重写
+        secure: false // 参考: https://webpack.js.org/configuration/dev-server/#devserverproxy
+      }
+    }
   },
 
   // https://webpack.js.org/concepts/loaders/
@@ -38,36 +50,33 @@ const config = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env']
+          presets: ["@babel/preset-env"]
         }
       },
       {
         test: /\.css$/i,
         use: [
-          'style-loader',
-          'css-loader'
+          "style-loader",
+          "css-loader"
           // Please note we are not running postcss here
         ]
       },
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ["style-loader", "css-loader", "sass-loader"]
       },
       {
         // Load all images as base64 encoding if they are smaller than 8192 bytes
         test: /\.(png|jpg|gif|svg|jpeg)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
+
             options: {
               // On development we want to see where the file is coming from, hence we preserve the [path]
-              name: '[path][name].[ext]?hash=[hash:20]',
+              name: "[path][name].[ext]?hash=[hash:20]",
               limit: 8192
             }
           }
@@ -77,9 +86,7 @@ const config = {
   },
 
   // https://webpack.js.org/concepts/plugins/
-  plugins: [
-    ...htmls()
-  ]
-}
-console.log(config.devServer.port)
-module.exports = config
+  plugins: [...htmls()]
+};
+console.log(config.devServer.port);
+module.exports = config;
